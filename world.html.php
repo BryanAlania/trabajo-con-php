400daa60598bd8a9bb8a1ff58ae8c7761a63cb2a
BryanAlania <1359537@senati.pe>
2022-10-27 18:51:12 -0500

    algo es algo profe

---
+++ world.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title> hecho en php</title>
+</head>
+<body>
+    <h1>hola mundo con php</h1>
+    <?php print("hola mundo desde este server") ?>
+
+    <h2>esto en html</h2>
+
+    <?php print("<p>esto lo escribir con un interprete de php</p>") ?>
+
+    <p>ya estaba escrito en html</p>
+
+    <?php
+        echo "<p>soy el comando echo</p>"
+        ?>
+        
+        <p><a> <?php print("volver") ?></a></p>
+</body>
+</html>
